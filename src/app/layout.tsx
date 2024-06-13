bfc5673e9c4c6372a8ae861a505583072580ed42
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-13 09:20:41 +0900

    Add: 내비게이션&페이지 새로고침을 위한 공통 <header> 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/Navbar";
+import ReloadButton from "@/components/ReloadButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex items-center justify-between p-3 border-b border-b-teal-500">
+          <NavBar />
+          <ReloadButton />
+        </header>
+        <section className="p-4">{children}</section>
+      </body>
     </html>
   );
 }
