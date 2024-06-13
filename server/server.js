45663cc207f8ac05e4eb75aaf7003114a2e6b289
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-13 09:09:28 +0900

    Add: 테스트용 API 작성

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let todos = [
+  { id: 1, text: "1. learn react", completed: true },
+  { id: 2, text: "2. learn next.js app router", completed: false },
+  { id: 3, text: "3. understand next.js caching", completed: false },
+];
+
+app.get("/todos", (_, res) => {
+  console.log("Received GET request at /todos");
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  console.log("Received POST request at /todos, id:", req.body.id);
+  const reqId = req.body.id;
+  todos = [...todos].forEach((todo) =>
+    todo.id === reqId ? { ...todo, completed: !todo.completed } : todo
+  );
+  res.json(todos);
+});
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
