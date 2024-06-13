2e0e798a8d62b8b928059ad93b9e95c7c3b1afc5
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-13 09:16:01 +0900

    Add: 타임스탬프 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <>
-      <h1>Caching</h1>
+      <h1>Learn Next.js Caching</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
     </>
   );
 }
