7ec5f806c85ba5d410498d90ef0ec1bbbf2d235b
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-13 09:16:20 +0900

    Create: 페이지 생성&타임스탬프 추가

---
+++ src/app/a/page.tsx
@@ -0,0 +1,8 @@
+export default function PageA() {
+  return (
+    <>
+      <h1>Page A</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </>
+  );
+}
---
+++ src/app/b/page.tsx
@@ -0,0 +1,8 @@
+export default function PageB() {
+  return (
+    <>
+      <h1>Page B</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </>
+  );
+}
